810d2800b29654617fca971faceddab4bda3a7e5
doochik <doochik@ya.ru>
2010-09-14 10:39:40 +0400

    Rectangle optimization

--- Jigsaws/Rectangle.js
+++ Jigsaws/Rectangle.js
@@ -4,90 +4,25 @@
     {
             name: 'rectangle',
 
-            _transparentData: document.createElement('canvas').getContext('2d').createImageData(1,1),
-
-            /**
-             * @param canvas
-             * @param x1
-             * @param y1
-             * @param x2
-             * @param y2
-             * @param female
-             */
-            _makeTransparent: function(canvas, x1, y1, x2, y2, female) {
-                var context = canvas.getContext('2d'),
-                    i, j;
-
-                for (i = x1; i < x2; i++) {
-                    for (j = y1; j < y2; j++) {
-                        // bitwise XOR
-                        // inPath ^ true === 0
-                        // !inPath ^ false === 0
-                        if ((context.isPointInPath(i, j) ^ female) === 0) {
-                            context.putImageData(this._transparentData, i, j);
-                        }
-                    }
-                }
-            },
-
-
-            /**
-             * Draw rectangle.
-             * x1,y1 -------> x2,y1
-             *                |
-             *                |
-             *               \/
-             * x1,y2 <------- x2,y2
-             * @param canvasContext
-             * @param x1
-             * @param y1
-             * @param x2
-             * @param y2
-             */
-            _rectH: function(canvasContext, x1, y1, x2, y2) {
-                canvasContext.beginPath();
-                canvasContext.moveTo(x1, y1);
-                canvasContext.lineTo(x2, y1);
-                canvasContext.lineTo(x2, y2);
-                canvasContext.lineTo(x1, y2);
-                canvasContext.strokeStyle = 'transparent';
-                canvasContext.stroke();
-                canvasContext.closePath();
-            },
-
-            /**
-             * Draw rectangle.
-             * x1,y1          x2,y1
-             *   |             /\
-             *   |             |
-             *  \/             |
-             * x1,y2 -------> x2,y2
-             * @param canvasContext
-             * @param x1
-             * @param y1
-             * @param x2
-             * @param y2
-             */
-            _rectV: function(canvasContext, x1, y1, x2, y2) {
-                canvasContext.beginPath();
-                canvasContext.moveTo(x1, y1);
-                canvasContext.lineTo(x1, y2);
-                canvasContext.lineTo(x2, y2);
-                canvasContext.lineTo(x2, y1);
-                canvasContext.strokeStyle = 'transparent';
-                canvasContext.stroke();
-                canvasContext.closePath();
-            },
-
             /**
              * Размер.
              * @type Number
              */
             _size: 0.2,
 
-            //TODO: не вырезать дату полного изображения
-            //TODO: не бегать по всей длине/высоте
-            //TODO: offsetX считается по-разному
+            _relationSide: function(type, x1, y1, x2, y2) {
+                return {
+                    type: type,
+                    offsetX: Math.round((x2 - x1) / 2 + x1),
+                    offsetY: Math.round((y2 - y1) / 2 + y1)
+                };
+            },
+
+            _makeFemale: function(canvas, x1, y1, x2, y2) {
+                var context = canvas.getContext('2d');
+                context.clearRect(x1, y1, x2 - x1, y2 - y1);
+            },
+
             male_0: function(canvas, width, height, x, y) {
                 var rw = width / 3,
                     x1 = Math.round(rw + x),
@@ -95,48 +30,39 @@
                     x2 = Math.round(rw*2 + x),
                     y2 = Math.round(height * this._size);
 
-                this._rectV(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, 0, 0, canvas.width, y2, false);
+                var context = canvas.getContext('2d');
+                context.clearRect(0, y1, x1, y2 - y1);
+                context.clearRect(x2, y1, canvas.width - x2, y2 - y1);
 
-                return {
-                    type: 'male',
-                    offsetX: Math.round((x1 - x2) / 2 + x2),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('male', x1, y1, x2, y2);
             },
 
             male_1: function(canvas, width, height, x, y) {
                 var rh = height / 3,
-                    x1 = Math.round(canvas.width),
+                    x1 = Math.round(width + x),
                     y1 = Math.round(rh + y),
-                    x2 = Math.round(width + x),
+                    x2 = Math.round(canvas.width),
                     y2 = Math.round(rh*2 + y);
 
-                this._rectH(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, x2, 0, canvas.width, canvas.height, false);
+                var context = canvas.getContext('2d');
+                context.clearRect(x1, 0, x2 - x1, y1);
+                context.clearRect(x1, y2, x2 - x1, canvas.height - y2);
 
-                return {
-                    type: 'male',
-                    offsetX: Math.round((x1 - x2) / 2 + x2),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('male', x1, y1, x2, y2);
             },
 
             male_2: function(canvas, width, height, x, y) {
                 var rw = width / 3,
                     x1 = Math.round(rw + x),
-                    y1 = Math.round(canvas.height),
+                    y1 = Math.round(height + y),
                     x2 = Math.round(rw*2 + x),
-                    y2 = Math.round(height + y);
+                    y2 = Math.round(canvas.height);
 
-                this._rectV(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, 0, y2, canvas.width, canvas.height, false);
+                var context = canvas.getContext('2d');
+                context.clearRect(0, y1, x1, y2 - y1);
+                context.clearRect(x2, y1, canvas.width - x2, y2 - y1);
 
-                return {
-                    type: 'male',
-                    offsetX: Math.round((x1 - x2) / 2 + x2),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('male', x1, y1, x2, y2);
             },
 
             male_3: function(canvas, width, height, x, y) {
@@ -146,14 +72,11 @@
                     x2 = Math.round(width * this._size),
                     y2 = Math.round(rh*2 + y);
 
-                this._rectH(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, 0, 0, x2, canvas.height, false);
+                var context = canvas.getContext('2d');
+                context.clearRect(x1, 0, x2 - x1, y1);
+                context.clearRect(x1, y2, x2 - x1, canvas.height - y2);
 
-                return {
-                    type: 'male',
-                    offsetX: Math.round((x1 - x2) / 2 + x2),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('male', x1, y1, x2, y2);
             },
 
             female_0: function(canvas, width, height, x, y) {
@@ -163,48 +86,33 @@
                     x2 = Math.round(rw*2 + x),
                     y2 = Math.round(height * this._size + y);
 
-                this._rectV(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, 0, 0, canvas.width, y2, true);
+                this._makeFemale(canvas, x1, y1, x2, y2);
 
-                return {
-                    type: 'female',
-                    offsetX: Math.round((x2 - x1) / 2 + x1),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('female', x1, y1, x2, y2);
             },
 
             female_1: function(canvas, width, height, x, y) {
                 var rh = height / 3,
-                    x1 = Math.round(width + x),
+                    x1 = Math.round(width * (1 - this._size) + x),
                     y1 = Math.round(rh + y),
-                    x2 = Math.round(width * (1 - this._size) + x),
+                    x2 = Math.round(width + x),
                     y2 = Math.round(rh*2 + y);
 
-                this._rectH(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, x2, 0, canvas.width, canvas.height, true);
+                this._makeFemale(canvas, x1, y1, x2, y2);
 
-                return {
-                    type: 'female',
-                    offsetX: Math.round((x2 - x1) / 2 + x1),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('female', x1, y1, x2, y2);
             },
 
             female_2: function(canvas, width, height, x, y) {
                 var rw = width / 3,
                     x1 = Math.round(rw + x),
-                    y1 = Math.round(height + y),
+                    y1 = Math.round(height * (1 - this._size) + y),
                     x2 = Math.round(rw*2 + x),
-                    y2 = Math.round(height * (1 - this._size) + y);
+                    y2 = Math.round(height + y);
 
-                this._rectV(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, 0, y2, canvas.width, canvas.height, true);
+                this._makeFemale(canvas, x1, y1, x2, y2);
 
-                return {
-                    type: 'female',
-                    offsetX: Math.round((x2 - x1) / 2 + x1),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('female', x1, y1, x2, y2);
             },
 
             female_3: function(canvas, width, height, x, y) {
@@ -214,14 +122,9 @@
                     x2 = Math.round(width * this._size + x),
                     y2 = Math.round(rh*2 + y);
 
-                this._rectH(canvas.getContext('2d'), x1, y1, x2, y2);
-                this._makeTransparent(canvas, 0, 0, x2, canvas.height, true);
+                this._makeFemale(canvas, x1, y1, x2, y2);
 
-                return {
-                    type: 'female',
-                    offsetX: Math.round((x2 - x1) / 2 + x1),
-                    offsetY: Math.round((y2 - y1) / 2 + y1)
-                };
+                return this._relationSide('female', x1, y1, x2, y2);
             },
 
             getSize: function(size) {

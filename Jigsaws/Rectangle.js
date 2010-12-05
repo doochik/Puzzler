e2982c75f9dffa1ea9f5df326344dd6d00453e12
doochik <doochik@ya.ru>
2010-12-05 18:20:18 +0300

    common functions

--- Jigsaws/Rectangle.js
+++ Jigsaws/Rectangle.js
@@ -22,17 +22,43 @@
             context.clearRect(x1, y1, x2 - x1, y2 - y1);
         },
 
+        /**
+         * Make male for top and bottom side.
+         * @param {HTMLCanvasElement} canvas
+         * @param {Number} x1
+         * @param {Number} y1
+         * @param {Number} x2
+         * @param {Number} y2
+         */
+        _makeMaleTopAndBottom: function(canvas, x1, y1, x2, y2) {
+            var context = canvas.getContext('2d');
+
+            context.clearRect(0, y1, x1, y2 - y1);
+            context.clearRect(x2, y1, canvas.width - x2, y2 - y1);
+        },
+
+        /**
+         * Make male for left and right side.
+         * @param {HTMLCanvasElement} canvas
+         * @param {Number} x1
+         * @param {Number} y1
+         * @param {Number} x2
+         * @param {Number} y2
+         */
+        _makeMaleLeftAndRight: function(canvas, x1, y1, x2, y2) {
+            var context = canvas.getContext('2d');
+            context.clearRect(x1, 0, x2 - x1, y1);
+            context.clearRect(x1, y2, x2 - x1, canvas.height - y2);
+        },
+
         male_0: function(canvas, width, height, x, y) {
             var rw = width / 3,
                 x1 = Math.round(rw + x),
                 y1 = 0,
                 x2 = Math.round(rw * 2 + x),
-                y2 = Math.round(height * this._size);
+                y2 = Math.round(y);
 
-            var context = canvas.getContext('2d');
-            context.clearRect(0, y1, x1, y2 - y1);
-            context.clearRect(x2, y1, canvas.width - x2, y2 - y1);
-
+            this._makeMaleTopAndBottom(canvas, x1, y1, x2, y2);
             return this._relationSide('male', x1, y1, x2, y2);
         },
 
@@ -43,10 +69,7 @@
                 x2 = Math.round(canvas.width),
                 y2 = Math.round(rh * 2 + y);
 
-            var context = canvas.getContext('2d');
-            context.clearRect(x1, 0, x2 - x1, y1);
-            context.clearRect(x1, y2, x2 - x1, canvas.height - y2);
-
+            this._makeMaleLeftAndRight(canvas, x1, y1, x2, y2);
             return this._relationSide('male', x1, y1, x2, y2);
         },
 
@@ -57,10 +80,7 @@
                 x2 = Math.round(rw * 2 + x),
                 y2 = Math.round(canvas.height);
 
-            var context = canvas.getContext('2d');
-            context.clearRect(0, y1, x1, y2 - y1);
-            context.clearRect(x2, y1, canvas.width - x2, y2 - y1);
-
+            this._makeMaleTopAndBottom(canvas, x1, y1, x2, y2);
             return this._relationSide('male', x1, y1, x2, y2);
         },
 
@@ -71,10 +91,7 @@
                 x2 = Math.round(width * this._size),
                 y2 = Math.round(rh * 2 + y);
 
-            var context = canvas.getContext('2d');
-            context.clearRect(x1, 0, x2 - x1, y1);
-            context.clearRect(x1, y2, x2 - x1, canvas.height - y2);
-
+            this._makeMaleLeftAndRight(canvas, x1, y1, x2, y2);
             return this._relationSide('male', x1, y1, x2, y2);
         },
 
@@ -86,7 +103,6 @@
                 y2 = Math.round(height * this._size + y);
 
             this._makeFemale(canvas, x1, y1, x2, y2);
-
             return this._relationSide('female', x1, y1, x2, y2);
         },
 
@@ -98,7 +114,6 @@
                 y2 = Math.round(rh * 2 + y);
 
             this._makeFemale(canvas, x1, y1, x2, y2);
-
             return this._relationSide('female', x1, y1, x2, y2);
         },
 
@@ -110,7 +125,6 @@
                 y2 = Math.round(height + y);
 
             this._makeFemale(canvas, x1, y1, x2, y2);
-
             return this._relationSide('female', x1, y1, x2, y2);
         },
 
@@ -122,7 +136,6 @@
                 y2 = Math.round(rh * 2 + y);
 
             this._makeFemale(canvas, x1, y1, x2, y2);
-
             return this._relationSide('female', x1, y1, x2, y2);
         },
 
